184155a007e18e6910b302d95f96a4ebb09e0fa6
AkhilSharma78148 <iamakhilsharma@gmail.com>
2021-01-03 15:54:40 +0530

    Understanding the Component Selector by element,attribute anc class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', //By elements
+  // selector: '[app-servers]', //By attribute
+  // selector: '.app-servers', //By class
   // templateUrl: './servers.component.html',
   template: `<app-server></app-server>
   			<app-server></app-server>`,
